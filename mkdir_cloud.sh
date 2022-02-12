1ffe582ede89b8033335b0210292fa7f5abcd9c6
anapraxis <84584585+anapraxis@users.noreply.github.com>
2022-02-12 17:06:39 +0300

    Create mkdir_cloud.sh

---
+++ mkdir_cloud.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+## Создаем структуру локальных папок для монтирования облаков
+mkdir -p /Volumes/Data/clouds/GDisk/nost79/root/15GB_nost79
+mkdir -p /Volumes/Data/clouds/GDisk/nost79/root/secret_15GB_nost79
+mkdir -p /Volumes/Data/clouds/GDisk/akuby333/root/15GB_akuby333
+mkdir -p /Volumes/Data/clouds/GDisk/akuby333/root/secret_15GB_akuby333
+mkdir -p /Volumes/Data/clouds/GDisk/nost79/unlim/unlim_yaakoubi_edu
+mkdir -p /Volumes/Data/clouds/GDisk/nost79/unlimsecret_unlim_yaakoubi_edu
+mkdir -p /Volumes/Data/clouds/mail_ru/100GB_mailru_nost79
+mkdir -p /Volumes/Data/clouds/mail_ru/secret_100GB_mailru_nost79
+mkdir -p /Volumes/Data/clouds/mail_ru/8GB_mailru_m_apostolov
+mkdir -p /Volumes/Data/clouds/mail_ru/secret_8GB_mailru_m_apostolov
+mkdir -p /Volumes/Data/clouds/yandex/280GB_yandex_mapostolov
+mkdir -p /Volumes/Data/clouds/yandex/secret_280GB_yandex_mapostolov
+mkdir -p /Volumes/Data/clouds/yandex/10GB_yandex_mikhail_apostolov_pro
+mkdir -p /Volumes/Data/clouds/MEGA/50GB_MEGA
+mkdir -p /Volumes/Data/clouds/MEGA/secret_50GB_MEGA
+mkdir -p /Volumes/Data/clouds/onedrive/5GB_onedrive_nost79
+mkdir -p /Volumes/Data/clouds/onedrive/5TB_onedrive_cmannino1
+mkdir -p /Volumes/Data/clouds/onedrive/5TB_onedrive_akuby333
+mkdir -p /Volumes/Data/clouds/onedrive/5TB_onedrive_mikhail2
+mkdir -p /Volumes/Data/clouds/nextcloud/anapraxis
