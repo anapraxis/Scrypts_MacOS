764130bbd5860f9b36b0322bbb9f8eece3e3333c
anapraxis <84584585+anapraxis@users.noreply.github.com>
2022-02-14 09:07:33 +0000

    Update chmod_ssh_key.sh

--- chmod_ssh_key.sh
+++ chmod_ssh_key.sh
@@ -1,2 +1,4 @@
-chmod 700 /Volumes/Data/Nextcloud/_key/ssh
+#!/usr/bin/env bash
+
+chmod 700 /Volumes/Data/Nextcloud/_key/ssh && 
 chmod 600 /Volumes/Data/Nextcloud/_key/ssh//2222_root@192.168.1.2/id_rsa
